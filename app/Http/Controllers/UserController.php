3ba52a626bf63eb4514e1d74320d7391338d1f86
LudoPilot <e.berardfr@gmail.com>
2025-05-10 00:14:50 +0200

    Create UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $users = User::with('address')
+            ->when($request->search, fn ($query, $search) =>
+                $query->where('first_name', 'like', "%{$search}%")
+                      ->orWhere('last_name', 'like', "%{$search}%")
+                      ->orWhere('email', 'like', "%{$search}%")
+            )
+			->orderBy('last_name')
+            ->paginate(20)
+            ->withQueryString();
+
+        return Inertia::render('Dashboard', [
+            'users' => $users,
+            'filters' => $request->only('search'),
+        ]);
+    }
+}
