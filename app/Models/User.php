bd5ad48a9fe3269e5051f4c1eaedbac599c8a649
LudoPilot <e.berardfr@gmail.com>
2025-05-10 19:58:28 +0200

    Correct the column name in the hasRole relationship in the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,6 +67,6 @@ class User extends Authenticatable
      */
     public function hasRole(string $roleName): bool
     {
-        return $this->roles()->where('name', $roleName)->exists();
+        return $this->roles()->where('role_name', $roleName)->exists();
     }
 }
