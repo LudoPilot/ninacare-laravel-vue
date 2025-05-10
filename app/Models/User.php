981d23a729eacc7392c163f7f3748d514de6c039
LudoPilot <e.berardfr@gmail.com>
2025-05-10 18:57:54 +0200

    Create the role_user table (via a migration) and add the adequate relationships in the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,21 @@ class User extends Authenticatable
     	return $this->belongsTo(Address::class);
 	}
 
+    /**
+     * Relation with roles (many-to-many)
+     */
+	public function roles() {
+		return $this->belongsToMany(Role::class);
+	}
+
+	/**
+     * Check if the user has a specific role
+     *
+     * @param string $roleName
+     * @return bool
+     */
+    public function hasRole(string $roleName): bool
+    {
+        return $this->roles()->where('name', $roleName)->exists();
+    }
 }
