30fbdbfd4b46481016d102aaca198254a7079d1a
LudoPilot <e.berardfr@gmail.com>
2025-05-10 02:30:05 +0200

    Add two buttons on the user details

--- resources/js/Pages/Users/Show.vue
+++ resources/js/Pages/Users/Show.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { usePage } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
 
 const props = defineProps<{
     user: {
@@ -19,26 +19,45 @@ const props = defineProps<{
 
 <template>
     <div class="p-6">
-        <h1 class="text-2xl font-bold mb-4">User Details</h1>
+        <div class="flex items-center justify-between mb-4">
+            <h1 class="text-2xl font-bold">User Details</h1>
+            <div class="space-x-2">
+                <!-- ✅ Bouton Retour -->
+                <Link
+                    href="/users"
+                    class="inline-block px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300"
+                >
+                    Back to List
+                </Link>
+
+                <!-- ✅ Bouton Modifier -->
+                <Link
+                    :href="`/users/${user.id}/edit`"
+                    class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                >
+                    Edit User
+                </Link>
+            </div>
+        </div>
 
         <div class="bg-white p-6 rounded shadow space-y-4">
             <div>
-                <b>First name:</b> {{ user.first_name }}
+                <strong>First Name:</strong> {{ user.first_name }}
             </div>
             <div>
-                <b>Last name:</b> {{ user.last_name }}
+                <strong>Last Name:</strong> {{ user.last_name }}
             </div>
             <div>
-                <b>Email:</b> {{ user.email }}
+                <strong>Email:</strong> {{ user.email }}
             </div>
 
             <div v-if="user.address">
                 <hr class="my-4" />
                 <h2 class="text-lg font-semibold">Address</h2>
-                <div><b>Street:</b> {{ user.address.street }}</div>
-                <div><b>City:</b> {{ user.address.city }}</div>
-                <div><b>Post Code:</b> {{ user.address.post_code }}</div>
-                <div><b>Country:</b> {{ user.address.country }}</div>
+                <div><strong>Street:</strong> {{ user.address.street }}</div>
+                <div><strong>City:</strong> {{ user.address.city }}</div>
+                <div><strong>Post Code:</strong> {{ user.address.post_code }}</div>
+                <div><strong>Country:</strong> {{ user.address.country }}</div>
             </div>
         </div>
     </div>
