51255f9285c72f3b614cc15491d3c85dc6cf0941
LudoPilot <e.berardfr@gmail.com>
2025-05-11 00:28:39 +0200

    Create a route for the About page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/about', function () {
+    return Inertia::render('About');
+})->name('about');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
